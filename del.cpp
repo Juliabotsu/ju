798813b62e41610c7a1ff8a290d3c21a7f7eb955
Juliabotsu <59306041+Juliabotsu@users.noreply.github.com>
2019-12-31 16:16:06 +0200

    Add files via upload
    Программа рандоммного соответствия, если рандомное число совпадает и введенным то вы выиграли если нет то вы лост

---
+++ del.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+	srand(time(0));
+	int numbery;
+	int magic;
+	magic= rand()%100;
+	std::cout <<"\t"<< "Input your number "<<"\n";
+	std::cin >> numbery;
+	std::cout <<"\t"<<"Magic number is "<< magic << '\n';
+	if (magic==numbery) {
+		std::cout << "You won " << '\n';
+	}
+	else
+	std::cout << "You loose" << '\n';
+ if (numbery<magic) {
+ 	std::cout << "Your number less than magic number" << '\n';
+ }
+ else
+ std::cout << "Your number more than magic number" << '\n';
+	return 0;
+}
